6aa026c07dc4b3f0c14d553ab4a1f7a98628cd76
sindhureddy111 <sindhureddy148@gmail.com>
2018-05-29 22:02:11 -0400

    second commit

---
+++ AutoFinance.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.4.18;
+
+contract FinanceBank{
+
+  //For list of documents required by bank
+  //1. Add the customer
+  //2.Check if all the documents are submitted by the customer for verfication
+  //3.Make all documents required(fil the form)
+  //4.After submission & verification, route from bank to the product company
+
+  //Initialize variables  
+
+  bytes32[] public owner;
+  bytes32[] public customerNames; 
+
+  uint amount;
+
+  //Start
+  //1.Add the customer to the system and verify if the customer already exists
+    function createIfNotExist(bytes32 _customerName) public{
+	
+	bool customerExist = false;
+	unit i = 0;
+	for(i=0; i <customerNames.length; i++){
+	  if(candidat)
+	}	
+
+      }
+  
+  
+
+}
